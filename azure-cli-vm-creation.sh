7671450a7596e817da3dfcde762e4f8b9d63f2c0
Pankaj Singh <pkstiyara@gmail.com>
2024-02-27 21:49:40 +0530

    Update azure-cli-vm-creation.sh

--- azure-cli-vm-creation.sh
+++ azure-cli-vm-creation.sh
@@ -11,3 +11,22 @@ az vm create \
     --admin-username azure \
     --admin-password Password@123 \
     --os-disk-size-gb 128
+
+
+
+
+#Red Hat Enterprise Linux 8.7 (LVM) - Gen2
+
+# Fetch current resource group
+resource_group=$(az group list --query '[0].name' -o tsv)
+
+# Create VM using the fetched resource group and RHEL image
+az vm create \
+    --resource-group $resource_group \
+    --name rhel1 \
+    --location eastus \
+    --image RedHat:RHEL:87-gen2:latest \
+    --size Standard_D2s_v3 \
+    --admin-username azure \
+    --admin-password Password@123 \
+    --os-disk-size-gb 128
