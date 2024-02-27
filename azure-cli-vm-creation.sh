5ad19d783824c497b86d17888999454673eef155
Pankaj Singh <pkstiyara@gmail.com>
2024-02-27 21:42:24 +0530

    Update azure-cli-vm-creation.sh

--- azure-cli-vm-creation.sh
+++ azure-cli-vm-creation.sh
@@ -1,19 +1,13 @@
+# Fetch current resource group
+resource_group=$(az group list --query '[0].name' -o tsv)
+
+# Create VM using the fetched resource group
 az vm create \
-    --resource-group 1-9b5c7497-playground-sandbox \
+    --resource-group $resource_group \
     --name vm1 \
     --location eastus \
     --image Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest \
     --size Standard_D2s_v3 \
     --admin-username azure \
-    --admin-password Password \
-    --os-disk-size-gb 128 \
-
-az vm create \
-    --resource-group 1-9b5c7497-playground-sandbox \
-    --name vm2 \
-    --location eastus \
-    --image Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest \
-    --size Standard_D2s_v3 \
-    --admin-username azure \
-    --admin-password Password \
-    --os-disk-size-gb 128 \
+    --admin-password Password@123 \
+    --os-disk-size-gb 128
